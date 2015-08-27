61279fdfe84412299e3e52df2bf807ee0034888b
Joao Rodrigues <jlrodrigues87@gmail.com>
2015-08-27 01:54:57 +0100

    removed dnsspoof and pointed to googles resolver

--- setup.sh
+++ setup.sh
@@ -7,14 +7,6 @@ function validate_var() {
 	fi
 }
 
-function get_or_else() {
-	if [ -n "$1" ]; then
-		echo "$1"
-	else
-		echo "$2"
-	fi
-}
-
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ "$#" -ne 1 ]; then
@@ -33,20 +25,7 @@ source $1
 # validation
 validate_var "DEB_CACHE_IP_ADDR" $DEB_CACHE_IP_ADDR
 validate_var "DEB_CACHE_IP_MASK" $DEB_CACHE_IP_MASK
-DEB_CACHE_DEB_MIR=`get_or_else $DEB_CACHE_DEB_MIR  ftp.us.debian.org`
-DEB_CACHE_DEB_SMIR=`get_or_else $DEB_CACHE_DEB_SMIR security.debian.org`
-DEB_CACHE_UBU_MIR=`get_or_else $DEB_CACHE_UBU_MIR  us.archive.ubuntu.com`
-DEB_CACHE_UBU_SMIR=`get_or_else $DEB_CACHE_UBU_SMIR security.ubuntu.com`
 
 # generate files
 $DIR/templates/tpl_vagrantfile.sh ${DEB_CACHE_IP_ADDR} > $DIR/Vagrantfile
-$DIR/templates/tpl_apt.sh ${DEB_CACHE_IP_MASK} \
-							${DEB_CACHE_DEB_MIR} \
-							${DEB_CACHE_DEB_SMIR} \
-							${DEB_CACHE_UBU_MIR} \
-							${DEB_CACHE_UBU_SMIR} > $DIR/conf/apt
-$DIR/templates/tpl_dnsspoof.sh ${DEB_CACHE_IP_ADDR} \
-							${DEB_CACHE_DEB_MIR} \
-							${DEB_CACHE_DEB_SMIR} \
-							${DEB_CACHE_UBU_MIR} \
-							${DEB_CACHE_UBU_SMIR} > $DIR/conf/dnsspoof.etc
+$DIR/templates/tpl_apt.sh ${DEB_CACHE_IP_MASK} > $DIR/conf/apt
--- templates/tpl_vagrantfile.sh
+++ templates/tpl_vagrantfile.sh
@@ -14,28 +14,17 @@ cat <<END
 
 \$provision_script = <<SCRIPT
   apt-get update
-  apt-get install -y nginx dsniff bind9
+  apt-get install -y nginx
 
   # create apt folders
-  mkdir -p /srv/www/apt/debian /srv/www/apt/debian-security /srv/www/apt/ubuntu
-  chown www-data:www-data -R /srv/www/apt
-  cd /srv/www
-  ln -s /srv/www/apt/debian .
-  ln -s /srv/www/apt/debian-security .
-  ln -s /srv/www/apt/ubuntu .
+  mkdir -p /srv/www/debian /srv/www/debian-security /srv/www/cache/partial
+  chown www-data:www-data -R /srv/www
 
   # configure nginx
   cp /vagrant/conf/apt /etc/nginx/sites-available/apt
   ln -s /etc/nginx/sites-available/apt /etc/nginx/sites-enabled/
   
-  cp /vagrant/conf/dnsspoof.initd /etc/init.d/dnsspoof
-  chmod 755 /etc/init.d/dnsspoof
-  update-rc.d dnsspoof defaults
-  cp /vagrant/conf/dnsspoof.default /etc/default/dnsspoof
-  
   /etc/init.d/nginx restart
-  /etc/init.d/dnsspoof stop
-  /etc/init.d/dnsspoof start
 SCRIPT
 
 Vagrant.configure(2) do |config|
